6c8eb1f202d78e13920f7cedba58805f79a2ccac
Francisco Etizel <franciscoetizel@gmail.com>
2022-10-04 17:04:00 -0300

    teste_!

--- js/script.js
+++ js/script.js
@@ -1,55 +1,86 @@
 //Active nav-menu
+// const links = document.querySelectorAll(".header-menu a");
+
+// function activeLink(link) {
+//   const url = location.href;
+//   const href = link.href;
+
+//   if (url.includes(href)) {
+//     link.classList.add("active");
+//   }
+// }
+
+// links.forEach(activeLink);
+
+// Active itemns of budget
+
+// const parametros = new URLSearchParams(location.search);
+
+// function activeProduct(parametro) {
+//   const element = document.getElementById(parametro);
+
+//   if (element) {
+//     element.checked = true;
+//   }
+// }
+
+// parametros.forEach(activeProduct);
+
+// Perguntas Frequentes
+
+// const perguntas = document.querySelectorAll(".perguntas button");
+
+// function perguntaSetada(cliked) {
+//   const local = cliked.currentTarget;
+//   const controls = local.getAttribute("aria-controls");
+//   const resposta = document.getElementById(controls);
+//   console.log(resposta);
+//   resposta.classList.toggle("active");
+//   const ativa = resposta.classList.contains("active");
+//   console.log(ativa);
+//   local.setAttribute("aria-expanded", "true");
+// }
+
+// function eventosPerguntas(pergunta) {
+//   pergunta.addEventListener("click", perguntaSetada);
+// }
+
+// perguntas.forEach(eventosPerguntas);
+
+// Galeria Dinâmica
+
+// const galeria = document.querySelectorAll(".bicicleta-imagens img");
+// const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+// function trocarImagem(event) {
+//   const img = event.currentTarget;
+//   const media = matchMedia("(min-width: 1000px)").matches;
+//   console.log(media);
+//   if (media) {
+//     galeriaContainer.prepend(img);
+//   }
+// }
+
+// function percorrerGaleria(img) {
+//   img.addEventListener("click", trocarImagem);
+// }
+
+// galeria.forEach(percorrerGaleria);
+
+// Animation Pluguin
+// if (window.SimpleAnime) {
+//   new SimpleAnime();
+// }
+
 const links = document.querySelectorAll(".header-menu a");
 
-function activeLink(link) {
-  const url = location.href;
+function linksEvent(link) {
   const href = link.href;
+  const url = location.href;
 
   if (url.includes(href)) {
     link.classList.add("active");
   }
 }
 
-links.forEach(activeLink);
-
-// Active itemns of budget
-
-const parametros = new URLSearchParams(location.search);
-
-function activeProduct(parametro) {
-  const element = document.getElementById(parametro);
-
-  if (element) {
-    element.checked = true;
-  }
-}
-
-parametros.forEach(activeProduct);
-
-// Perguntas Frequentes
-
-const perguntas = document.querySelectorAll(".perguntas button");
-
-function perguntaSetada(cliked) {
-  const local = cliked.currentTarget;
-  const controls = local.getAttribute("aria-controls");
-  const resposta = document.getElementById(controls);
-  console.log(resposta);
-  resposta.classList.toggle("active");
-  const ativa = resposta.classList.contains("active");
-  console.log(ativa);
-  local.setAttribute("aria-expanded", "true");
-}
-
-function eventosPerguntas(pergunta) {
-  pergunta.addEventListener("click", perguntaSetada);
-}
-
-perguntas.forEach(eventosPerguntas);
-
-// Galeria Dinâmica
-
-const galeria = document.querySelectorAll(".bicicleta-imagens img");
-const galeriaContainer = document.querySelector(".bicicleta-imagens");
-
-console.log(galeria, galeriaContainer);
+links.forEach(linksEvent);
